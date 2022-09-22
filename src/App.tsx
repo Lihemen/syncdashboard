1b882b75ca133e44aa0417260eff5bdf712b7124
Hemense Lan <lihemen@gmail.com>
2022-09-22 11:00:04 +0100

    assets (#5) (#6)
    Co-authored-by: Hemense Lan <hlan@afexnigeria.com>
    
    Co-authored-by: Hemense Lan <hlan@afexnigeria.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello</h1>
+    <div>
+      <h1 className='underline text-3xl'>Hello</h1>
     </div>
   );
 }
