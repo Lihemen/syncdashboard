84249e9cd27c2230e98e41a8d72e61ae767af64b
Hemense Lan <lihemen@gmail.com>
2022-09-22 11:17:55 +0100

    Merge Dev to staging (#9) (#10)
    * initial project structure with components
    
    * assets (#5)
    
    Co-authored-by: Hemense Lan <hlan@afexnigeria.com>
    
    Co-authored-by: Hemense Lan <hlan@afexnigeria.com>
    
    Co-authored-by: Hemense Lan <hlan@afexnigeria.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,49 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ['Open Sans', 'sans-serif'],
+      body: ['Open Sans', 'sans-serif'],
+    },
+    extend: {
+      fontSize: {
+        14: '14px',
+      },
+      backgroundColor: {
+        'main-bg': '#fafbfb',
+        'main-dark-bg': '#20232a',
+        'secondary-dark-bg': '#33373e',
+        'light-gray': '#f7f7f7',
+        'half-transparent': 'rgba(0, 0, 0, 0.5)',
+      },
+      borderWidth: {
+        1: '1px',
+      },
+      borderColor: {
+        color: 'rgba(0,0,0,0.1)',
+      },
+      width: {
+        400: '400px',
+        760: '760px',
+        780: '780px',
+        800: '800px',
+        1000: '1000px',
+        1200: '1200px',
+        1400: '1400px',
+      },
+      height: {
+        80: '80px',
+      },
+      minHeight: {
+        590: '590px',
+      },
+      backgroundImage: {
+        'hero-pattern':
+          "url('https://demos.wrappixel.com/premium-admin-templates/react/flexy-react/main/static/media/welcome-bg-2x-svg.25338f53.svg')",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
-}
+};
+
